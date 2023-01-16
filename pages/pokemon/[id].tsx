95f89c820d7d1f4f6c58c95695722f2cf8462ced
Nacho <nacho75xr@gmail.com>
2023-01-16 20:32:31 +0100

    Section 6: ISR and ISG

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -119,17 +119,29 @@ export const getStaticPaths: GetStaticPaths = async (_ctx) => {
     paths: pokemons151.map((id) => ({
       params: { id },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    revalidate: 86400,
   };
 };
 
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,13 +2,17 @@ import { pokeApi } from '../api';
 import { Pokemon } from '../interfaces';
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const {
-    data: { id, name, sprites },
-  } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  try {
+    const {
+      data: { id, name, sprites },
+    } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-  return {
-    id,
-    name,
-    sprites,
-  };
+    return {
+      id,
+      name,
+      sprites,
+    };
+  } catch (error) {
+    return null;
+  }
 };
